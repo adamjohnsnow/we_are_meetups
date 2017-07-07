2f81418377befde56515682c7668919e774a4455
Adam Snow <adamjohnsnow@icloud.com>
2017-07-07 14:40:57 +0100

    better email formatting

--- models/email.rb
+++ models/email.rb
@@ -1,8 +1,12 @@
 require 'net/smtp'
+require_relative './linkedin'
 
 class Email
 attr_reader :status
+
   def self.send(invite_address)
+    @email_account = LinkedInAuth.get(2)
+    
     msg = <<END_OF_MESSAGE
 From: Peter Kerwood's Amazing Events <your@mail.address>
 To: #{invite_address}
@@ -13,8 +17,8 @@ END_OF_MESSAGE
 
     smtp = Net::SMTP.new 'smtp.gmail.com', 587
     smtp.enable_starttls
-    smtp.start('www.gmail.com', 'snowdonialimited@gmail.com', 'b33chw00d', :login) do
-      smtp.send_message(msg, 'snowdonialimited@gmail.com', invite_address)
+    smtp.start('www.gmail.com', @email_account.client_id, @email_account.client_secret, :login) do
+      smtp.send_message(msg, @email_account.client_id, invite_address)
     end
   end
 
