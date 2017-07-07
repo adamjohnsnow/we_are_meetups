8983cbf176fb4f4d6f65945fc00a4b274222d161
Adam Snow <adamjohnsnow@icloud.com>
2017-07-07 13:56:07 +0100

    better email formatting

--- models/email.rb
+++ models/email.rb
@@ -3,7 +3,14 @@ require 'net/smtp'
 class Email
 attr_reader :status
   def self.send(invite_address)
-    msg = "Subject: An invite from Marketing Superstore\n\nIf you can read this, it works!!"
+    msg = <<END_OF_MESSAGE
+From: Peter Kerwood's Amazing Events <your@mail.address>
+To: #{invite_address}
+Subject: You have been invited to our next amazing event!
+
+There will be a link to follow here...
+END_OF_MESSAGE
+
     smtp = Net::SMTP.new 'smtp.gmail.com', 587
     smtp.enable_starttls
     smtp.start('www.gmail.com', 'snowdonialimited@gmail.com', 'b33chw00d', :login) do
