fdb66b052f80fd20384d943f1ae6da8c44df6270
Adam Snow <adamjohnsnow@icloud.com>
2017-07-14 15:36:10 +0100

    edit email link

--- models/linkedin.rb
+++ models/linkedin.rb
@@ -8,7 +8,7 @@ class LinkedInAuth
   property :client_secret, String
 
   REDIRECT_LOGIN = 'http://localhost:9292'
-  HEROKU = "https://marketing-superstore-events.herokuapp.com"
+  HEROKU = "https://wearemeetups.herokuapp.com"
   ENV['RACK_ENV'] == 'development' ? HOSTNAME = REDIRECT_LOGIN : HOSTNAME = HEROKU
 
 end
