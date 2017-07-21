a022861c206643a5b66f65f50699728b418bc8c2
Adam Snow <adamjohnsnow@icloud.com>
2017-07-21 09:34:48 +0100

    email methods updated for new account

--- models/email.rb
+++ models/email.rb
@@ -69,7 +69,7 @@ attr_reader :status
 
     mailer = Mailit::Mailer.new
 
-    mailer.send(mail, :server => 'smtp.123-reg.co.uk', :port => 465,
+    mailer.send(mail, :server => 'smtp.123-reg.co.uk', :port => 587,
       :domain => '123-reg.co.uk', :password => ENV['EMAIL_PASSWORD'])
     end
 end
